57b8e193efea80bf378199473b7c92da3749197f
agirnob <kakcayigitbaran@gmail.com>
2022-05-15 14:43:30 +0300

    Initialize commit

---
+++ src/main/java/com/tau/rest3d/Rest3dApplication.java
@@ -0,0 +1,13 @@
+package com.tau.rest3d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rest3dApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rest3dApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tau/rest3d/Rest3dApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tau.rest3d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rest3dApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
